f6fe5a2f5694c1427d3610159c0fa86221ae327a
k-gitest <86330971+k-gitest@users.noreply.github.com>
2024-06-05 22:47:00 +0900

    Delete bootstrap/ssr directory

--- bootstrap/ssr/assets/isoDateGenerator-zYlad1ZJ.js
+++
@@ -1,8 +0,0 @@
-const isoDateGenerator = () => {
-  const currentDate = /* @__PURE__ */ new Date();
-  const oneDayAgo = new Date(currentDate.getTime() - 1 * 24 * 60 * 60 * 1e3);
-  return oneDayAgo.toISOString();
-};
-export {
-  isoDateGenerator as i
-};
--- bootstrap/ssr/assets/supabase-B-jbb2wE.js
+++
@@ -1,6 +0,0 @@
-const supabaseURL = "https://rjxtqhgniojmrcynyqkh.supabase.co/storage/v1/object/public/";
-const supabaseNoImage = "images/noimage.png";
-export {
-  supabaseNoImage as a,
-  supabaseURL as s
-};
