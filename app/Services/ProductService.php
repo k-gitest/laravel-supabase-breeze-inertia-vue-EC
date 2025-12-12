fb88e2dfb854bb4404412f7aa4e3bb3c4b18792a
k-gitest <adobe_blow@yahoo.co.jp>
2025-12-12 12:48:30 +0900

    96th commit

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -9,6 +9,7 @@ use App\Models\Favorite;
 use App\Models\ProductVector;
 use Pgvector\Laravel\Distance;
 use Illuminate\Support\Facades\Auth;
+use Log;
 
 class ProductService
 {
@@ -22,35 +23,44 @@ class ProductService
 
     public function getProductDetails($id)
     {
-        $authCheck = Auth::check();
+        $product = Product::with(['category', 'image', 'comment', 'stock', 'favorite', 'vector'])
+            ->withSum('stock', 'quantity')
+            ->findOrFail($id);
+
+        # $authCheck = Auth::check();
+        $userId = Auth::id(); // Auth::check() && Auth::user()->id; と同等の動作
         $isInCart = false;
         $isInComment = false;
         $isInFavorite = false;
 
-        if ($authCheck) {
-            $userId = auth()->user()->id;
+        if ($userId) {
+            # $userId = auth()->user()->id;
             $isInCart = Cart::where('user_id', $userId)->where('product_id', $id)->exists();
             $isInComment = Comment::where('user_id', $userId)->where('product_id', $id)->exists();
             $isInFavorite = Favorite::where('user_id', $userId)->where('product_id', $id)->exists();
         }
 
-        $product = Product::with(['category', 'image', 'comment', 'stock', 'favorite', 'vector'])
-            ->withSum('stock', 'quantity')
-            ->findOrFail($id);
+        if (!$product->vector) {
+            // ログを記録（データ不備を通知）
+            Log::warning("Product ID {$id} is missing vector data. Cannot recommend neighbors.");
+            
+            // エラーにせず、空のコレクションを返してサービスを続行
+            $recommendedProducts = collect([]); 
+        } else {
+            $embedding = $product->vector->embedding;
 
-        $embedding = $product->vector->embedding;
+            $nearestNeighbors = ProductVector::query()
+                ->nearestNeighbors('embedding', $embedding, Distance::L2)
+                ->where('product_id', '!=', $id) 
+                ->take(5)
+                ->get();
 
-        $nearestNeighbors = ProductVector::query()
-            ->nearestNeighbors('embedding', $embedding, Distance::L2)
-            ->where('product_id', '!=', $id) 
-            ->take(5)
-            ->get();
-
-        $productIds = $nearestNeighbors->pluck('product_id');
-        $recommendedProducts = Product::whereIn('id', $productIds)
-            ->with(['category', 'image', 'stock', 'favorite'])
-            ->withSum('stock', 'quantity')  
-            ->get();
+            $productIds = $nearestNeighbors->pluck('product_id');
+            $recommendedProducts = Product::whereIn('id', $productIds)
+                ->with(['category', 'image', 'stock', 'favorite', 'comment'])
+                ->withSum('stock', 'quantity')  
+                ->get();
+        }
 
         return [
             'product' => $product,
